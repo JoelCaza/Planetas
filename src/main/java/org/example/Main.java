106bcb2059883b32eeed2601d7f919bbbe1137f5
Joel <joelcaza@hotmail.com>
2024-05-09 09:58:29 -0500

    Commit v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresa nombre:");
+        System.out.println("Ingresa nombre.:");
         String nombre = sc.nextLine();
         System.out.println("Ingresa cantidad satelites:");
         int satelites = sc.nextInt();
